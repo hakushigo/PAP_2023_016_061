d3eb7a4220152688cf6ee79577f6cabdc31d322e
Johanna Siagian <johannasiagian3@gmail.com>
2023-12-08 20:37:35 +0700

    delete

--- Lembaga/script.js
+++
@@ -1,9 +0,0 @@
-function reveal(what){
-    let elowhat = document.getElementById(what);
-    elowhat.style.display = 'block';
-}
-
-function hide(what){
-    let elowhat = document.getElementById(what);
-    elowhat.style.display = 'none';
-}
